ed8026facab7505718a383f167661e6872d36d6f
Amine El Ghayate <aelghayate@simplon.co>
2017-10-24 12:16:15 +0200

    update

--- wp-content/themes/zerif-lite-child/sections/lots.php
+++ wp-content/themes/zerif-lite-child/sections/lots.php
@@ -12,14 +12,14 @@
 
         <div class="row lots-items">
             <div class="col-lg-4">
-                <img src="/wp-content/uploads/2017/10/220051-L-LO.jpg" alt="..." class="img-thumbnail">
+            <div class="image" style="background-image:url(/wp-content/uploads/2017/10/220051-L-LO.jpg">&nbsp;</div>
                 <h3>Google Home</h3>
             </div>
             <div class="col-lg-4">
-                <img src="/wp-content/uploads/2017/10/220051-L-LO.jpg" alt="..." class="img-thumbnail">
+                <div class="image" style="background-image:url(/wp-content/themes/zerif-lite-child/images/studio.jpg)">&nbsp;</div>
                 <h3>Visite du studio Ubisoft</h3></div>
             <div class="col-lg-4">
-                <img src="/wp-content/uploads/2017/10/220051-L-LO.jpg" alt="..." class="img-thumbnail">
+            <div class="image" style="background-image:url(/wp-content/uploads/2017/10/220051-L-LO.jpg)">&nbsp;</div>
                 <h3>Abonnement Netflix</h3>
             </div>
         </div>
